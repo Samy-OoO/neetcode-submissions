8cd784b607a83f3de15230d7c1189bc219ca86d6
Samy <samyakshakya172@gmail.com>
2026-09-20 10:33:32 +0545

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        res = new LinkedList<>();
+        backtrack(0, new LinkedList<>(), nums);
+        return res;
+    }
+
+    private void backtrack(int i, List<Integer> subset, int[] nums) {
+        if (i >= nums.length) {
+            List<Integer> copy = new LinkedList(subset);
+            Collections.sort(copy);
+            if (!res.contains(copy)) res.add(copy);
+            return;
+        }
+
+        subset.add(nums[i]);
+        backtrack(i+1, subset, nums);
+
+        subset.removeLast();
+        backtrack(i+1, subset, nums);
+    }
+}
