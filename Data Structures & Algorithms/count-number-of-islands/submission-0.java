6c0f4d70fdcb87273d6e8e7cad86a269ffde3ca3
Samy <samyakshakya172@gmail.com>
2026-09-25 16:58:32 +0545

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    private static final int[][] directions = { {1, 0},{-1, 0},{0, 1}, {0, -1}};
+    int rows, cols;
+
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) return 0;
+
+        rows = grid.length;
+        cols = grid[0].length;
+        int islands = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1') {
+                    bfs(grid, r, c);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+
+    private void bfs(char[][] grid, int r, int c) {
+        Queue<int[]> q = new LinkedList<>();
+        grid[r][c] = '0';
+        q.add(new int[]{r, c});
+
+        while (!q.isEmpty()) {
+            int[] node = q.poll();
+            int row = node[0], col = node[1];
+
+            for (int[] dir : directions) {
+                int nr = row + dir[0], nc = col + dir[1];
+
+                if ( (nr >= 0 && nr < rows) &&
+                     (nc >= 0 && nc < cols) &&
+                     grid[nr][nc] == '1' ) {
+                        q.add(new int[]{nr, nc});
+                        grid[nr][nc] = '0';
+                }
+            }
+        }
+    }
+}
